5c5bac7e83de1f9adae07c579d1a53870d38c47d
unknown <gregorysantini@luminadex.com>
2023-08-01 19:11:05 +0300

    fix:landing page design

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Carousel } from "antd";
 import { Pie } from "@ant-design/charts";
 import { BsCurrencyExchange } from "react-icons/bs";
 import { MdGeneratingTokens, MdGroups3 } from "react-icons/md";
@@ -71,12 +71,38 @@ function HomePage() {
     <div className="w-full mt-16">
       {/* page1 */}
       <div className="flex flex-col gap-4 justify-center">
-        <div className="w-full flex justify-center">
-          <div
-            className="w-[838px] h-[478px]"
-            style={{ backgroundImage: "url(/banner/1.jpg)" }}
-          ></div>
-        </div>
+        <Carousel autoplay effect="fade">
+          <div>
+            <div
+              className="w-full h-[500px]"
+              style={{
+                backgroundImage: "url(/banner/banner2.jpg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%",
+              }}
+            ></div>
+          </div>
+          <div>
+            <div
+              className="w-full h-[500px]"
+              style={{
+                backgroundImage: "url(/banner/banner1.jpg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%",
+              }}
+            ></div>
+          </div>
+          <div>
+            <div
+              className="w-full h-[500px]"
+              style={{
+                backgroundImage: "url(/banner/banner.jpg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "100% 100%",
+              }}
+            ></div>
+          </div>
+        </Carousel>
 
         <div className="font-bold text-light-100 text-6xl">
           The future of finance is in plain sight.
@@ -94,26 +120,26 @@ function HomePage() {
         </div>
       </div>
       {/* page2 */}
-      <div className="container mx-auto mt-10 flex flex-col gap-4 p-10 text-light-100">
-        <div className="text-3xl">
+      <div className="container mx-auto mt-10 flex flex-col gap-4 text-light-100">
+        <div className="text-4xl">
           Lumina is a zero-knowledge, KYC-enabled, enterprise-ready DEX on Mina
         </div>
-        <div className="flex flex-row justify-between flex-wrap">
-          <div className="flex flex-col gap-3 items-center basis-1/3">
+        <div className="flex flex-row justify-center flex-wrap">
+          <div className="flex flex-col gap-3 items-center basis-1/3 px-5">
             <div>Clarity </div>
             <div className="rounded-full border-2 border-light-200 p-8 shadow-md shadow-blue-500/50">
               <BsCurrencyExchange className="text-8xl" />
             </div>
             <div>Description 1</div>
           </div>
-          <div className="flex flex-col gap-3 items-center basis-1/3">
+          <div className="flex flex-col gap-3 items-center basis-1/3 px-5">
             <div>Clarity </div>
             <div className="rounded-full border-2 border-light-200 p-8 shadow-md shadow-blue-500/50">
               <MdGeneratingTokens className="text-8xl" />
             </div>
             <div>Description 1</div>
           </div>
-          <div className="flex flex-col gap-3 items-center basis-1/3">
+          <div className="flex flex-col gap-3 items-center basis-1/3 px-5">
             <div>Clarity </div>
             <div className="rounded-full border-2 border-light-200 p-8 shadow-md shadow-blue-500/50">
               <MdGroups3 className="text-8xl" />
@@ -123,29 +149,29 @@ function HomePage() {
         </div>
       </div>
       {/* page3 */}
-      <div className="container mx-auto flex flex-col gap-4 mt-10 text-light-100">
-        <div className="text-3xl">
+      <div className="container mx-auto flex flex-col gap-6 mt-10 text-light-100">
+        <div className="text-4xl">
           Backed by the top names in Blockchain and zk! (Press release)
         </div>
-        <div className="flex flex-row justify-between items-center flex-wrap">
-          <div className="basis-1/4 px-16">
+        <div className="flex flex-row justify-center items-center flex-wrap">
+          <div className="flex flex-col gap-3 items-center basis-1/4 px-5">
             <div className="cursor-pointer h-20 flex justify-center items-center p-8 rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50">
-              <img src="/top/1.svg" loading="lazy"></img>
+              <img src="/top/1.svg" loading="lazy" alt=""></img>
             </div>
           </div>
-          <div className="basis-1/4 px-16">
+          <div className="flex flex-col gap-3 items-center basis-1/4 px-5">
             <div className="cursor-pointer h-20 flex justify-center items-center p-8 rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50">
-              <img src="/top/2.svg" loading="lazy"></img>
+              <img src="/top/2.svg" loading="lazy" alt=""></img>
             </div>
           </div>
-          <div className="basis-1/4 px-16">
+          <div className="flex flex-col gap-3 items-center basis-1/4 px-5">
             <div className="cursor-pointer h-20 flex justify-center items-center p-8 rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50">
-              <img src="/top/3.svg" loading="lazy"></img>
+              <img src="/top/3.svg" loading="lazy" alt=""></img>
             </div>
           </div>
-          <div className="basis-1/4 px-16">
+          <div className="flex flex-col gap-3 items-center basis-1/4 px-5">
             <div className="cursor-pointer h-20 flex justify-center items-center p-8 rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50">
-              <img src="/top/4.svg" loading="lazy"></img>
+              <img src="/top/4.svg" loading="lazy" alt=""></img>
             </div>
           </div>
         </div>
@@ -158,8 +184,8 @@ function HomePage() {
         </div>
       </div>
       {/* page4 */}
-      <div className="container mx-auto flex flex-col gap-4 mt-10 text-light-100">
-        <div className="text-3xl">
+      <div className="container mx-auto flex flex-col gap-6 mt-10 text-light-100">
+        <div className="text-4xl">
           Collaborating with the best in the industry!
         </div>
         <div className="flex flex-row justify-between">
@@ -168,6 +194,7 @@ function HomePage() {
               src="/investors/ascentive-assets.svg"
               loading="lazy"
               className="h-11"
+              alt="ascentive-assets"
             ></img>
           </div>
           <div className="cursor-pointer rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50 p-5 h-20">
@@ -175,6 +202,7 @@ function HomePage() {
               src="/investors/cfund.svg"
               loading="lazy"
               className="h-11"
+              alt="cfund"
             ></img>
           </div>
           <div className="cursor-pointer rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50 p-5 h-20">
@@ -182,6 +210,7 @@ function HomePage() {
               src="/investors/kronos.svg"
               loading="lazy"
               className="h-11"
+              alt="kronos"
             ></img>
           </div>
           <div className="cursor-pointer rounded-lg border-2 border-light-200 shadow-md shadow-blue-500/50 p-5 h-20">
@@ -189,13 +218,14 @@ function HomePage() {
               src="/investors/morning-star.svg"
               loading="lazy"
               className="h-11"
+              alt="morning-star"
             ></img>
           </div>
         </div>
       </div>
       {/* page5 */}
       <div className="container mx-auto flex flex-col gap-4 mt-10">
-        <div className="text-light-100 text-3xl">
+        <div className="text-light-100 text-4xl">
           Sign up for the Journey, and get early updates on the Roadmap!
         </div>
         <div>
@@ -213,10 +243,10 @@ function HomePage() {
         <div></div>
       </div>
       {/* page6 */}
-      <div className="container mx-auto flex flex-col gap-4 mt-10 text-light-100">
+      <div className="container mx-auto flex flex-col gap-6 mt-10 text-light-100">
         <div className="text-4xl">Early builders</div>
-        <div className="flex flex-row justify-between flex-wrap">
-          <div className="flex flex-col gap-1 pr-10 basis-1/4">
+        <div className="flex flex-row justify-center flex-wrap">
+          <div className="flex flex-col gap-1 basis-1/4">
             <div className="flex flex-col gap-3 items-center">
               <div className="w-32 h-32 rounded-full bg-light-200"></div>
               <div className="">
@@ -224,14 +254,14 @@ function HomePage() {
                 <span>CEO,DB</span>
               </div>
             </div>
-            <div>
+            <div className="text-center">
               <ul className="list-disc">
                 <li>Node, Ubeswap, Celo, Terra</li>
                 <li>FRBNY, Treasury, White House, BofA</li>
               </ul>
             </div>
           </div>
-          <div className="flex flex-col gap-1 px-5 basis-1/4">
+          <div className="flex flex-col gap-1 basis-1/4">
             <div className="flex flex-col gap-3 items-center">
               <div className="w-32 h-32 rounded-full bg-light-200"></div>
               <div className="">
@@ -239,14 +269,14 @@ function HomePage() {
                 <span>CEO,DB</span>
               </div>
             </div>
-            <div>
+            <div className="text-center">
               <ul className="list-disc">
                 <li>Node, Ubeswap, Celo, Terra</li>
                 <li>FRBNY, Treasury, White House, BofA</li>
               </ul>
             </div>
           </div>
-          <div className="flex flex-col gap-1 px-5 basis-1/4">
+          <div className="flex flex-col gap-1 basis-1/4">
             <div className="flex flex-col gap-3 items-center">
               <div className="w-32 h-32 rounded-full bg-light-200"></div>
               <div className="">
@@ -254,14 +284,14 @@ function HomePage() {
                 <span>CEO,DB</span>
               </div>
             </div>
-            <div>
+            <div className="text-center">
               <ul className="list-disc">
                 <li>Node, Ubeswap, Celo, Terra</li>
                 <li>FRBNY, Treasury, White House, BofA</li>
               </ul>
             </div>
           </div>
-          <div className="flex flex-col gap-1 pl-10 basis-1/4">
+          <div className="flex flex-col gap-1 basis-1/4">
             <div className="flex flex-col gap-3 items-center">
               <div className="w-32 h-32 rounded-full bg-light-200"></div>
               <div className="">
@@ -269,7 +299,7 @@ function HomePage() {
                 <span>CEO,DB</span>
               </div>
             </div>
-            <div>
+            <div className="text-center">
               <ul className="list-disc">
                 <li>Node, Ubeswap, Celo, Terra</li>
                 <li>FRBNY, Treasury, White House, BofA</li>
@@ -279,21 +309,31 @@ function HomePage() {
         </div>
       </div>
       {/* page7 */}
-      <div className="container mx-auto flex flex-col gap-4 mt-10 text-light-100">
-        <div className="text-3xl">
+      <div className="container mx-auto flex flex-col gap-6 mt-10 text-light-100">
+        <div className="text-4xl">
           The Big Opportunity for Lumina: Migrating TradFi to DeFi
         </div>
         <div>
           <Pie {...config} />
         </div>
-        <div className="w-full flex justify-center">
-          <div
-            className="w-[838px] h-[478px]"
-            style={{ backgroundImage: "url(/banner/1.jpg)" }}
-          ></div>
+      </div>
+      {/* page8 */}
+      <div className="container mx-auto flex flex-col gap-6 mt-10 text-light-100 mb-10">
+        <div className="text-4xl">The Technology Stack</div>
+        <div className="text-left text-2xl">
+          Permissionless and Permissioned Markets
         </div>
-        {/* pipe chart */}
-        <div>{/* <PieChartComponent /> */}</div>
+        <div className="flex justify-center">
+          <img src="/1/2.jpg" loading="lazy" alt=""></img>
+        </div>
+        <div className="text-left text-2xl">Privacy and MEV Resistance</div>
+        <div className="flex justify-center">
+          <img src="/1/1.jpg" loading="lazy" alt=""></img>
+        </div>
+        <div className="text-left text-2xl">Scaling with zk-Rollups</div>
+        <div className="flex justify-center">
+          <img src="/1/3.jpg" loading="lazy" alt=""></img>
+        </div>
       </div>
     </div>
   );
